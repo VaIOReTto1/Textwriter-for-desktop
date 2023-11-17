f3278ee872c89684d7686e04cd52e3517e3ce4c0
VaIOReTto1 <1302140648@qq.com>
2023-11-17 16:34:27 +0800

    bugfix：修复多文档点击错误

--- src/main/kotlin/TextWriter.kt
+++ src/main/kotlin/TextWriter.kt
@@ -8,13 +8,14 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Transparent
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.times
+import java.awt.Toolkit
+import java.awt.datatransfer.DataFlavor
+import java.awt.datatransfer.StringSelection
 import java.io.File
 import javax.swing.JFileChooser
 import javax.swing.filechooser.FileNameExtensionFilter
@@ -30,7 +31,7 @@ fun TextWriterUi() {
     var currentDocumentIndex by remember { mutableStateOf(-1) }
 
     val defaultTextStyle = MaterialTheme.typography.body1
-    var textStyle by remember { mutableStateOf(defaultTextStyle) }
+    val textStyle by remember { mutableStateOf(defaultTextStyle) }
     val fileChooser = JFileChooser().apply {
         fileFilter = FileNameExtensionFilter("Text Files", "txt")
     }
@@ -71,8 +72,63 @@ fun TextWriterUi() {
                         }
                     }
                 },
-                onTextStyleChange = { newStyle -> textStyle = newStyle },
-                textStyle = textStyle,
+                onCopy = {
+                    if (currentDocumentIndex in documents.indices) {
+                        val currentContent = documents[currentDocumentIndex].content.text
+                        setClipboardContent(currentContent)
+                    }
+                },
+
+                onPaste = {
+                    if (currentDocumentIndex in documents.indices) {
+                        val clipboardContent = getClipboardContent()
+                        val currentDocument = documents[currentDocumentIndex]
+                        documents = documents.toMutableList().apply {
+                            this[currentDocumentIndex] =
+                                currentDocument.copy(content = TextFieldValue(clipboardContent))
+                        }
+                    }
+                },
+                onBold = {
+                    if (currentDocumentIndex in documents.indices) {
+                        val currentDocument = documents[currentDocumentIndex]
+                        val newText = applyMarkdownStyle(
+                            currentDocument.content.text,
+                            currentDocument.content.selection,
+                            "**"
+                        )
+                        documents = documents.toMutableList().apply {
+                            this[currentDocumentIndex] = currentDocument.copy(content = TextFieldValue(newText))
+                        }
+                    }
+                },
+
+                onItalic = {
+                    if (currentDocumentIndex in documents.indices) {
+                        val currentDocument = documents[currentDocumentIndex]
+                        val newText = applyMarkdownStyle(
+                            currentDocument.content.text,
+                            currentDocument.content.selection,
+                            "*"
+                        )
+                        documents = documents.toMutableList().apply {
+                            this[currentDocumentIndex] = currentDocument.copy(content = TextFieldValue(newText))
+                        }
+                    }
+                },
+                onUnderline = {
+                    if (currentDocumentIndex in documents.indices) {
+                        val currentDocument = documents[currentDocumentIndex]
+                        val newText = applyMarkdownStyle(
+                            currentDocument.content.text,
+                            currentDocument.content.selection,
+                            "_"
+                        )
+                        documents = documents.toMutableList().apply {
+                            this[currentDocumentIndex] = currentDocument.copy(content = TextFieldValue(newText))
+                        }
+                    }
+                },
             )
 
             if (currentDocumentIndex in documents.indices) {
@@ -92,6 +148,16 @@ fun TextWriterUi() {
     }
 }
 
+fun applyMarkdownStyle(text: String, selection: TextRange, styleSymbol: String): String {
+    if (selection.collapsed) return text
+
+    val before = text.substring(0, selection.start)
+    val selectedText = text.substring(selection.start, selection.end)
+    val after = text.substring(selection.end)
+
+    return before + styleSymbol + selectedText + styleSymbol + after
+}
+
 @Composable
 fun DocumentTabs(
     documents: List<Document>,
@@ -108,7 +174,7 @@ fun DocumentTabs(
                 Tab(
                     selected = index == currentDocumentIndex,
                     onClick = { onTabSelected(index) },
-                    text = { Text(document.file?.name ?: "未命名", overflow = TextOverflow.Ellipsis,maxLines = 1) },
+                    text = { Text(document.file?.name ?: "未命名", overflow = TextOverflow.Ellipsis, maxLines = 1) },
                     modifier = Modifier.padding(2.dp)
                         .border(color = Color.Gray, shape = RoundedCornerShape(4.dp), width = 1.dp).background(
                             Color.White
@@ -119,13 +185,27 @@ fun DocumentTabs(
     }
 }
 
+fun getClipboardContent(): String {
+    val clipboard = Toolkit.getDefaultToolkit().systemClipboard
+    return clipboard.getData(DataFlavor.stringFlavor) as String
+}
+
+fun setClipboardContent(content: String) {
+    val clipboard = Toolkit.getDefaultToolkit().systemClipboard
+    val data = StringSelection(content)
+    clipboard.setContents(data, data)
+}
+
 @Composable
 fun DocumentOperationsToolbar(
     onNew: () -> Unit,
     onOpen: () -> Unit,
     onSave: () -> Unit,
-    onTextStyleChange: (TextStyle) -> Unit,
-    textStyle: TextStyle,
+    onCopy: () -> Unit,
+    onPaste: () -> Unit,
+    onBold: () -> Unit,
+    onItalic: () -> Unit,
+    onUnderline: () -> Unit,
 ) {
     Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
         Button(onClick = onNew, colors = ButtonDefaults.buttonColors(Color(0xff708090))) {
@@ -137,17 +217,29 @@ fun DocumentOperationsToolbar(
         Button(onClick = onSave, colors = ButtonDefaults.buttonColors(Color(0xff708090))) {
             Text("保存")
         }
+        Button(onClick = onCopy, colors = ButtonDefaults.buttonColors(Color(0xff708090))) {
+            Text("复制")
+        }
+        Button(onClick = onPaste, colors = ButtonDefaults.buttonColors(Color(0xff708090))) {
+            Text("粘贴")
+        }
         Button(
-            onClick = { onTextStyleChange(textStyle.copy(fontWeight = FontWeight.Bold)) },
+            onClick = onBold,
             colors = ButtonDefaults.buttonColors(Color(0xff708090))
         ) {
             Text("黑体")
         }
         Button(
-            onClick = { onTextStyleChange(textStyle.copy(fontStyle = FontStyle.Italic)) },
+            onClick = onItalic,
             colors = ButtonDefaults.buttonColors(Color(0xff708090))
         ) {
             Text("斜体")
+        }
+        Button(
+            onClick = onUnderline,
+            colors = ButtonDefaults.buttonColors(Color(0xff708090))
+        ) {
+            Text("下划线")
         }
     }
 }
