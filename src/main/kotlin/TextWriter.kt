45c6bd73d1c40c6c9d7e2352b9c2d1adf3161403
VaIOReTto1 <1302140648@qq.com>
2023-11-23 14:56:57 +0800

    feature:增加关闭文档功能

--- src/main/kotlin/TextWriter.kt
+++ src/main/kotlin/TextWriter.kt
@@ -4,11 +4,16 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Transparent
-import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -20,44 +25,97 @@ import java.io.File
 import javax.swing.JFileChooser
 import javax.swing.filechooser.FileNameExtensionFilter
 
+//文本选择
 data class Document(
     val file: File?,
     var content: TextFieldValue
 )
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun TextWriterUi() {
-    var documents by remember { mutableStateOf(listOf<Document>()) }
-    var currentDocumentIndex by remember { mutableStateOf(-1) }
+    var documents by remember { mutableStateOf(listOf<Document>()) } // 文档列表
+    var currentDocumentIndex by remember { mutableStateOf(-1) } //当前文档索引
 
     val defaultTextStyle = MaterialTheme.typography.body1
-    val textStyle by remember { mutableStateOf(defaultTextStyle) }
+    var textStyle by remember { mutableStateOf(defaultTextStyle) }
+
+    var showCloseConfirmationDialog by remember { mutableStateOf(false) } // 显示关闭确认对话框
+    var closingDocumentIndex by remember { mutableStateOf(-1) } // 关闭文档索引
+
+    // 文档选择器
     val fileChooser = JFileChooser().apply {
         fileFilter = FileNameExtensionFilter("Text Files", "txt")
     }
 
+    fun closeTab(index: Int) {
+        showCloseConfirmationDialog = true
+        closingDocumentIndex = index
+    }
+
+
     MaterialTheme {
         Column(modifier = Modifier.padding(16.dp)) {
-            DocumentTabs(documents, currentDocumentIndex) { index ->
-                currentDocumentIndex = index
+            // 显示关闭确认对话框
+            if (showCloseConfirmationDialog) {
+                AlertDialog(
+                    onDismissRequest = { showCloseConfirmationDialog = false },
+                    title = { Text("关闭文档") },
+                    text = { Text("是否保存对文档的更改？") },
+                    confirmButton = {
+                        Button(
+                            onClick = {
+                                // 保存文档
+                                val closingDocument = documents[closingDocumentIndex]
+                                closingDocument.file?.writeText(closingDocument.content.text)
+                                // 从列表中移除并关闭对话框
+                                documents = documents.filterIndexed { i, _ -> i != closingDocumentIndex }
+                                if (closingDocumentIndex <= currentDocumentIndex) {
+                                    currentDocumentIndex = maxOf(0, currentDocumentIndex - 1)
+                                }
+                                showCloseConfirmationDialog = false
+                            }
+                        ) { Text("保存") }
+                    },
+                    dismissButton = {
+                        Button(
+                            onClick = {
+                                documents = documents.filterIndexed { i, _ -> i != closingDocumentIndex }
+                                if (closingDocumentIndex <= currentDocumentIndex) {
+                                    currentDocumentIndex = maxOf(0, currentDocumentIndex - 1)
+                                }
+                                showCloseConfirmationDialog = false
+                            }
+                        ) { Text("不保存") }
+                    }
+                )
             }
 
+            // 文档选项卡
+            DocumentTabs(documents, currentDocumentIndex, onTabSelected = { index ->
+                currentDocumentIndex = index
+            }, onCloseTab = ::closeTab)
+
+            // 文档操作工具栏
             DocumentOperationsToolbar(
                 onNew = {
                     val newDocument = Document(null, TextFieldValue())
                     documents = documents + newDocument
-                    currentDocumentIndex = documents.lastIndex // 确保更新为新文档的索引
+                    currentDocumentIndex = documents.lastIndex
                     println("${documents.size} + ${documents.lastIndex}")
                 },
 
+                //打开文档
                 onOpen = {
                     if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
                         val selectedFile = fileChooser.selectedFile
                         val newDocument = Document(selectedFile, TextFieldValue(selectedFile.readText()))
                         documents = documents + newDocument
-                        currentDocumentIndex = documents.lastIndex // 确保更新为新文档的索引
+                        currentDocumentIndex = documents.lastIndex
                     }
                 },
+
+                //保存文档
                 onSave = {
                     currentDocumentIndex.takeIf { it >= 0 }?.let { index ->
                         val currentDocument = documents[index]
@@ -72,6 +130,8 @@ fun TextWriterUi() {
                         }
                     }
                 },
+
+                //复制
                 onCopy = {
                     if (currentDocumentIndex in documents.indices) {
                         val currentContent = documents[currentDocumentIndex].content.text
@@ -79,6 +139,7 @@ fun TextWriterUi() {
                     }
                 },
 
+                //粘贴
                 onPaste = {
                     if (currentDocumentIndex in documents.indices) {
                         val clipboardContent = getClipboardContent()
@@ -89,49 +150,13 @@ fun TextWriterUi() {
                         }
                     }
                 },
-                onBold = {
-                    if (currentDocumentIndex in documents.indices) {
-                        val currentDocument = documents[currentDocumentIndex]
-                        val newText = applyMarkdownStyle(
-                            currentDocument.content.text,
-                            currentDocument.content.selection,
-                            "**"
-                        )
-                        documents = documents.toMutableList().apply {
-                            this[currentDocumentIndex] = currentDocument.copy(content = TextFieldValue(newText))
-                        }
-                    }
-                },
 
-                onItalic = {
-                    if (currentDocumentIndex in documents.indices) {
-                        val currentDocument = documents[currentDocumentIndex]
-                        val newText = applyMarkdownStyle(
-                            currentDocument.content.text,
-                            currentDocument.content.selection,
-                            "*"
-                        )
-                        documents = documents.toMutableList().apply {
-                            this[currentDocumentIndex] = currentDocument.copy(content = TextFieldValue(newText))
-                        }
-                    }
-                },
-                onUnderline = {
-                    if (currentDocumentIndex in documents.indices) {
-                        val currentDocument = documents[currentDocumentIndex]
-                        val newText = applyMarkdownStyle(
-                            currentDocument.content.text,
-                            currentDocument.content.selection,
-                            "_"
-                        )
-                        documents = documents.toMutableList().apply {
-                            this[currentDocumentIndex] = currentDocument.copy(content = TextFieldValue(newText))
-                        }
-                    }
-                },
+                onTextStyleChange = { newStyle -> textStyle = newStyle }, //更改文本样式
+                textStyle = textStyle, //当前文本样式
             )
 
             if (currentDocumentIndex in documents.indices) {
+                //显示文本
                 val currentDocument = documents[currentDocumentIndex]
                 BasicTextField(
                     value = currentDocument.content,
@@ -148,21 +173,13 @@ fun TextWriterUi() {
     }
 }
 
-fun applyMarkdownStyle(text: String, selection: TextRange, styleSymbol: String): String {
-    if (selection.collapsed) return text
-
-    val before = text.substring(0, selection.start)
-    val selectedText = text.substring(selection.start, selection.end)
-    val after = text.substring(selection.end)
-
-    return before + styleSymbol + selectedText + styleSymbol + after
-}
-
+// 文档选项卡
 @Composable
 fun DocumentTabs(
     documents: List<Document>,
     currentDocumentIndex: Int,
-    onTabSelected: (index: Int) -> Unit
+    onTabSelected: (index: Int) -> Unit,
+    onCloseTab: (index: Int) -> Unit
 ) {
     if (documents.isNotEmpty()) {
         TabRow(
@@ -174,28 +191,46 @@ fun DocumentTabs(
                 Tab(
                     selected = index == currentDocumentIndex,
                     onClick = { onTabSelected(index) },
-                    text = { Text(document.file?.name ?: "未命名", overflow = TextOverflow.Ellipsis, maxLines = 1) },
+                    text = {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            Text(
+                                document.file?.name ?: "未命名",
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1,
+                                modifier = Modifier.weight(2f)
+                            )
+                            // 关闭按钮
+                            IconButton(onClick = { onCloseTab(index) }, modifier = Modifier.weight(1f)) {
+                                Icon(Icons.Default.Close, contentDescription = "Close")
+                            }
+                        }
+                    },
                     modifier = Modifier.padding(2.dp)
-                        .border(color = Color.Gray, shape = RoundedCornerShape(4.dp), width = 1.dp).background(
-                            Color.White
-                        )
+                        .border(color = Color.Gray, shape = RoundedCornerShape(4.dp), width = 1.dp)
+                        .background(Color.White)
                 )
             }
         }
     }
 }
 
+
+// 获取文本并复制
 fun getClipboardContent(): String {
     val clipboard = Toolkit.getDefaultToolkit().systemClipboard
     return clipboard.getData(DataFlavor.stringFlavor) as String
 }
 
+// 在文档中粘贴文本
 fun setClipboardContent(content: String) {
     val clipboard = Toolkit.getDefaultToolkit().systemClipboard
     val data = StringSelection(content)
     clipboard.setContents(data, data)
 }
 
+// 文档操作工具栏
 @Composable
 fun DocumentOperationsToolbar(
     onNew: () -> Unit,
@@ -203,43 +238,43 @@ fun DocumentOperationsToolbar(
     onSave: () -> Unit,
     onCopy: () -> Unit,
     onPaste: () -> Unit,
-    onBold: () -> Unit,
-    onItalic: () -> Unit,
-    onUnderline: () -> Unit,
+    onTextStyleChange: (TextStyle) -> Unit,
+    textStyle: TextStyle,
 ) {
+
     Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
         Button(onClick = onNew, colors = ButtonDefaults.buttonColors(Color(0xff708090))) {
             Text("新建")
         }
+
         Button(onClick = onOpen, colors = ButtonDefaults.buttonColors(Color(0xff708090))) {
             Text("打开")
         }
+
         Button(onClick = onSave, colors = ButtonDefaults.buttonColors(Color(0xff708090))) {
             Text("保存")
         }
+
         Button(onClick = onCopy, colors = ButtonDefaults.buttonColors(Color(0xff708090))) {
             Text("复制")
         }
+
         Button(onClick = onPaste, colors = ButtonDefaults.buttonColors(Color(0xff708090))) {
             Text("粘贴")
         }
+
         Button(
-            onClick = onBold,
+            onClick = { onTextStyleChange(textStyle.copy(fontWeight = FontWeight.Bold)) },
             colors = ButtonDefaults.buttonColors(Color(0xff708090))
         ) {
             Text("黑体")
         }
+
         Button(
-            onClick = onItalic,
+            onClick = { onTextStyleChange(textStyle.copy(fontStyle = FontStyle.Italic)) },
             colors = ButtonDefaults.buttonColors(Color(0xff708090))
         ) {
             Text("斜体")
-        }
-        Button(
-            onClick = onUnderline,
-            colors = ButtonDefaults.buttonColors(Color(0xff708090))
-        ) {
-            Text("下划线")
         }
     }
 }
